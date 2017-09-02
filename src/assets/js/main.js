eb4852038ad8fb632d2ab18b0ddc64176f26c45e
Fabien <fabien.webdev@gmail.com>
2017-09-02 10:18:56 +0900

    Refactor scroll calculs and improve scrolling experience

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,14 +2,24 @@ $(function() {
   // Scripts to run for JavaScript supported browsers
   $('.nojs.notice-message').addClass('hidden');
 
+  // Store elements we'll use to make parallax effects
+  var about = $('#about');
+  var html = $('#html');
+  var webdev = $('#webdev');
+  var learning = $('#learning');
+  var msgForm = $('#msgForm');
+  var nameInput = $('#name');
+  var emailInput = $('#email');
+  var msgInput = $('#msg');
+  var submitBtn = $('#submit');
+
   ////////////////////////
   // Smooth Scrolling
   ////////////////////////
 
   //@prepros-append scrollspy.js
 
-  // Manage active links while scrolling
-  gumshoe.init();
+
 
   ////////////////////////
   // Work section
@@ -28,6 +38,28 @@ $(function() {
   // layout Masonry after each image loads
   $grid.imagesLoaded().progress( function() {
     $grid.masonry();
+  });
+  // Update sections layout infos after Masonry finsihed rearranging images
+  var scrolls = {}
+  $grid.on( 'layoutComplete', function() { // Will update the scrolls value on each masonry update
+    console.log('layout done, just this one time');
+    var inView = $(window).height();
+    var offset = 200;
+    scrolls = {
+      about : (about.offset().top) - (about.height()) + offset,
+      aboutEnd : (about.offset().top + about.height()) - 50,
+      dot1 : (html.offset().top) - inView + offset* 0.75,
+      dot2 : (webdev.offset().top) - inView + offset* 0.75,
+      dot3 : (learning.offset().top) - inView + offset* 0.75,
+      form : msgForm.offset().top - inView,
+      name : nameInput.offset().top - inView + offset,
+      msg : nameInput.offset().top - inView + offset + nameInput.height(),
+      submit : submitBtn.offset().top - inView + offset/2,
+    }
+    // Manage active links while scrolling
+    gumshoe.init({
+      offset: 200
+    });
   });
 
   $('.grid-item').on('click', function(){
@@ -51,43 +83,27 @@ $(function() {
         }
       });
     }
-    var aboutScroll = ($('section#about').offset().top) + (($('section#about').height()) / 2) - 100;
-    var aboutEndScroll = ($('section#about').offset().top) + (($('section#about').height()) * 1.5) + 100;
-    var dotScroll1 = ($('#html').offset().top) + (($('#html').height()) / 2) - 200;
-    var dotScroll2 = ($('#webdev').offset().top) + (($('#webdev').height()) / 2) - 200;
-    var dotScroll3 = ($('#learning').offset().top) + (($('#learning').height()) / 2) - 200;
 
   ////////////////////////
   // Contact section
   ////////////////////////
     $('section#contact').removeClass('nojs');
-    var nameInput = $('#name');
-    var emailInput = $('#email');
-    var msgInput = $('#msg');
-    var submitBtn = $('#submit');
-    var formScroll = $('#msgForm').offset().top;
-    var nameScroll = nameInput.offset().top + ((nameInput.height()) / 2) - 200;
-    var msgScroll = msgInput.offset().top + ((msgInput.height()) / 2) - 200;
-    var submitScroll = submitBtn.offset().top + ((submitBtn.height()) / 2) - 200;
-
     var animateInputs = function() {
       nameInput.addClass('active');
       emailInput.addClass('active');
     }
-
     var animateMsg = function() {
       msgInput.addClass('active');
     }
-
     var animateBtn = function() {
       submitBtn.addClass('active');
     }
 
-
   // Scrolling effect on the whole page
   $(document).on('scroll', function(e){
     var scroll = $(this).scrollTop();
-    //console.log(scroll);
+    var inView = $(window).height();
+    console.log(scroll, inView);
     //Hide Sidebar when scroll more than 100px
     if (scroll > 100) {
       $('#sidebar').addClass('retracted');
@@ -104,39 +120,54 @@ $(function() {
     }
 
     // Animate About section between 2700 and 3850 ScrollTop
-    if (scroll > aboutScroll && scroll < aboutEndScroll) {
+    if (scroll > scrolls.about && scroll < scrolls.aboutEnd) {
       $('section#about').addClass('unwrapped');
     } else {
       $('section#about').removeClass('unwrapped');
       $('section#about .dot').removeClass('active');
     }
     // Animate Skills dots
-    if (scroll > dotScroll1) {
+    if (scroll > scrolls.dot1) {
       animateDot($('section#about #html .dots'), 9);
       animateDot($('section#about #js .dots'), 9);
     }
-    if (scroll > dotScroll2) {
+    if (scroll > scrolls.dot2) {
       animateDot($('section#about #webdev .dots'), 9);
       animateDot($('section#about #webdesign .dots'), 7);
     }
-    if (scroll > dotScroll3) {
+    if (scroll > scrolls.dot3) {
       animateDot($('section#about #organization .dots'), 8);
       animateDot($('section#about #learning .dots'), 10);
     }
-    if (scroll < formScroll) {
+    if (scroll < scrolls.form) {
       nameInput.removeClass('active');
       emailInput.removeClass('active');
       msgInput.removeClass('active');
       submitBtn.removeClass('active');
     }
-    if (scroll > nameScroll) {
+    if (scroll > scrolls.name) {
       animateInputs();
     }
-    if (scroll > msgScroll) {
+    if (scroll > scrolls.msg) {
       animateMsg();
     }
-    if (scroll > submitScroll) {
+    if (scroll > scrolls.submit) {
       animateBtn();
     }
+  })
+
+
+  console.log(scrolls);
+  $('.main-logo').on('click', function(e){
+    e.preventDefault();
+    console.log(scrolls);
+    var offset = $('#msgForm').offset().top;
+    var height = $('#msgForm').height();
+    var inView = $(window).height();
+    console.log('offset: ' + offset);
+    console.log('height: ' + height);
+    console.log('offset - height: ' + (offset - inView));
+
+    //console.log(($('section#about').offset().top) + (($('section#about').height()) / 2) - 100);
   })
 });
